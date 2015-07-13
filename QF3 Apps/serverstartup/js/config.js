6915b256a00d27961d4c59ff375286add103fcc7
Katherine ChengLi <katchengli@gmail.com>
2015-07-13 13:21:56 -0400

    Adding serverstartup app

---
+++ QF3 Apps/serverstartup/js/config.js
@@ -0,0 +1,10 @@
+define(function(){ 
+        var config = {};
+        config.app = 'serverstartup';
+        config.loginLocation = 'index.html';
+        config.extraScripts=[];
+        
+        config.dataTransferType = "text";
+        config.description = "Server Startup App";
+        return config;
+});
