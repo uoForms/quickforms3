534617d01386a2f3cd70de08a3ab2689b5b1116d
Kaifan Zhu <kzhu075@uottawa.ca>
2015-11-01 17:44:31 -0500

    CCM update

--- QF3 Apps/CCM/js/config.js
+++ QF3 Apps/CCM/js/config.js
@@ -1,10 +1,11 @@
    define(function(){
       var config = {};
-      config.app = 'pregapp'; // Your App name
-	  config.jqueryMobileTheme = "/francine/css/theme.css"; 
+      config.app = 'CCM'; // Your App name
+	  config.jqueryMobileTheme = "/CCM/css/theme.css"; 
 	  //config.jqueryMobilePath="/francine/js/jquery.mobile-1.2.0";
 	  //config.jqueryMobileCss="/francine/css/mobile.css";
-      config.extraScripts = [];
+	  config.loginLocation = 'index.html';
+	  config.extraScripts = ['js/authSaid.js'];
       config.quickformsEnding = ""; // "" or ".asp"
 	  config.defaultPageTransition = "none"; // slide, pop, none
 	  config.defaultDialogTransition = "pop"; // slide, pop, none	  
